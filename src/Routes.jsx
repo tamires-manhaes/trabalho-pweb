c53f6e5841e81e41dd564e05970143e672e69d41
Tamires <tamires.manhaes@ifba.edu.br>
2021-11-16 18:06:29 -0300

    chore: add pages for routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NotFound } from './pages/404';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login/index.';
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" element={() => <h1>home</h1>} />
+        <Route exact path="/" element={Home} />
+        <Route exact path="/login" element={Login} />
+        <Route element={NotFound} />
       </Switch>
     </BrowserRouter>
   );
