138de86731d41151e2ea43bb05105fa8450685f7
Tamires <tamires.manhaes@ifba.edu.br>
2021-11-17 19:07:12 -0300

    deleting wrong filenames

--- src/pages/404/index..jsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-  return (
-    <div className="404">
-      <span>Página não encontrada</span>
-    </div>
-  );
-};
--- src/pages/Login/index..jsx
+++
@@ -1,7 +0,0 @@
-export const Login = () => {
-  return (
-    <div className="loginPage">
-      <span>hello from login for client</span>
-    </div>
-  );
-};
