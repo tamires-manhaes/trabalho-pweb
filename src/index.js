61a4e0a42e1acc5d15c1d0dcbdaa49bbe88873b9
Tamires <tamires.manhaes@ifba.edu.br>
2021-11-28 14:53:59 -0300

    add rotas ao index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Routes } from './Routes';
+import Routes from './Routes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
