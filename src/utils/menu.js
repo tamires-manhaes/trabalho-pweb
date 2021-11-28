115af15dfda36f393b4235c5253d9f65e6b51659
Tamires <tamires.manhaes@ifba.edu.br>
2021-11-28 14:54:08 -0300

    add constantes do menu

---
+++ src/utils/menu.js
@@ -0,0 +1,9 @@
+const menu = [
+  { id: 0, title: 'home', path: '/home' },
+  { id: 1, title: 'nova reserva', path: '/new_reservation' },
+  { id: 2, title: 'ofertas', path: '/promotion' },
+  { id: 3, title: 'grupo de carros', path: '/car_groups' },
+  { id: 4, title: 'ajuda', path: '/help' },
+];
+
+export default menu;
