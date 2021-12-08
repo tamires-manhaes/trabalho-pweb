fc2b2ecc0e770f114b344b0202ef4ab35769e163
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-08 16:11:39 -0300

    chore: creating car card component

---
+++ src/components/CarCard/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CarCard = () => {
+  return (
+    <li className="carCard">
+      <h3>nome:</h3>
+      <span>cor:</span>
+      <span>qtd assentos:</span>
+      <span>kms rodados:</span>
+      <span>litros de gasolina:</span>
+      <span>disponivel:</span>
+    </li>
+  );
+};
+
+export default CarCard;
