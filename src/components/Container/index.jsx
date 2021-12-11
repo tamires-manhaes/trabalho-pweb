d957d7ca96b14afcfb228b5ad9e050afd73bbf59
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-11 17:40:18 -0300

    chore: changing prop name

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './styles.css';
 import Header from '../Header';
 
-const Container = ({ children, showMenu, title }) => {
+const Container = ({ children, showButton, title }) => {
   return (
     <div className="container">
-      <Header showMenu={showMenu} title={title} />
+      <Header showButton={showButton} title={title} />
       {children}
     </div>
   );
