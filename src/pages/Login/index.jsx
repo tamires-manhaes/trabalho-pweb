d5be41756556e1c5cd500ec52d8ba4645b0a28ad
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-11 17:38:45 -0300

    chore: basic estructure on login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,20 @@
-export const Login = () => {
+import Container from '../../components/Container';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const Login = () => {
   return (
-    <div className="loginPage">
-      <span>hello from login for client</span>
-    </div>
+    <Container title="Login">
+      <div className="loginPage">
+        <form className="formLogin">
+          <input type="text" placeholder="email ou cpf" />
+          <input type="password" placeholder="senha" />
+          <button type="submit">Entrar</button>
+          <Link to="/newUser">Novo(a) aqui? cadastre-se</Link>
+        </form>
+      </div>
+    </Container>
   );
 };
+
+export default Login;
