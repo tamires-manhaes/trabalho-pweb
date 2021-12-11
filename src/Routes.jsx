dfd77d43122ec9d592a06d8956daf95dfee7a57c
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-11 17:38:11 -0300

    chore: adding new routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Login } from './pages/Login';
+import Login from './pages/Login';
+import NewClient from './pages/NewClient';
+import NewUser from './pages/NewUser';
 import { NotFound } from './pages/Page404';
 
 const Routes = () => {
@@ -10,6 +12,10 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/newUser" component={NewUser} />
+        <Route exact path="/new/client" component={NewClient} />
+        <Route exact path="/new/employee" component={NotFound} />
+        <Route exact path="/new/car" component={NotFound} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
