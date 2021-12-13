4565f80fdf662758cb1f3f04f447a3526df7fddb
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-13 19:43:47 -0300

    chore: componente de card de infos

--- src/components/CarCard/index.jsx
+++ src/components/CarCard/index.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './styles.css';
 
-const CarCard = () => {
+const CarCard = ({
+  id,
+  name,
+  classificationGroup,
+  cor,
+  foto,
+  qtdSits,
+  kmsDriven,
+  qtdFuel,
+  available,
+}) => {
+  const history = useHistory();
+
+  const redirectToDetails = (id) => {
+    return history.push(`/car-details/${id}`);
+  };
+
   return (
     <li className="carCard">
-      <h3>nome:</h3>
-      <span>cor:</span>
-      <span>qtd assentos:</span>
-      <span>kms rodados:</span>
-      <span>litros de gasolina:</span>
-      <span>disponivel:</span>
+      <h3>{name}</h3>
+      <img src={foto} alt="car cover" />
+      <span>Cor: {cor}</span>
+      <span>Classificação: {classificationGroup.description}</span>
+      <span>qtd assentos: {qtdSits}</span>
+      <span>kms rodados: {kmsDriven}</span>
+      <span>litros de gasolina: {qtdFuel}</span>
+      <span>disponivel: {available}</span>
+      <button onClick={redirectToDetails}>Mais informações</button>
     </li>
   );
 };
