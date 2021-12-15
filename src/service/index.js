2a1fafc9fdd8478292f404e0ed1f34f8fd0f5135
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-15 10:50:02 -0300

    chore: api config

--- src/service/index.js
+++ src/service/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
+const token =
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYW9AZ21haWwuY29tIiwiZXhwIjoxNTk0MDYwMzE3fQ.5KlsXS917pSe3Tf_ORtmHLuFDpWR_RwqKuZGee0_fw72QRgE_s1K0K56LPd5_FAV8U-4a-RYBBE-iKig8uFtjw';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://pweb-locadora.herokuapp.com',
+  withCredentials: false,
   headers: {
+    Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json',
-    Accept: 'application/json',
+    'Access-Control-Allow-Origin': 'true',
   },
 });
 
