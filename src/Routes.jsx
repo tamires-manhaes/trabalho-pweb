12d75c483671396829b1a8b1c17bb37422221742
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-14 21:49:31 -0300

    chore: adicionando rotas de carro e reserva

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
 import Login from './pages/Login';
+import NewCar from './pages/NewCar';
 import NewClient from './pages/NewClient';
 import NewEmployee from './pages/NewEmployee';
+import NewReservation from './pages/NewReservation';
 import NewUser from './pages/NewUser';
 import { NotFound } from './pages/Page404';
 
@@ -16,7 +18,8 @@ const Routes = () => {
         <Route exact path="/newUser" component={NewUser} />
         <Route exact path="/new/client" component={NewClient} />
         <Route exact path="/new/employee" component={NewEmployee} />
-        <Route exact path="/new/car" component={NotFound} />
+        <Route exact path="/new/car" component={NewCar} />
+        <Route exact path="/new/reservation" component={NewReservation} />
         <Route exact path="/car-details/:id" component={NotFound} />
         <Route component={NotFound} />
       </Switch>
