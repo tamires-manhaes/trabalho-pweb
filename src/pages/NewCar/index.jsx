f660a437d5424c13ab7c573f25675465d7deb040
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-19 15:50:13 -0300

    chore: add new car fields

--- src/pages/NewCar/index.jsx
+++ src/pages/NewCar/index.jsx
@@ -10,6 +10,9 @@ const NewCar = () => {
   const [cor, setCor] = useState('');
   const [chassi, setChassi] = useState('');
   const [renavan, setRenavan] = useState('');
+  const [marca, setMarca] = useState('');
+  const [modelo, setModelo] = useState('');
+  const [qtdPortas, setQtdPortas] = useState('');
   const [tipoVeiculo, setTipoVeiculo] = useState('');
 
   const redirectToHome = () => {
@@ -25,6 +28,9 @@ const NewCar = () => {
       chassi,
       renavan,
       tipoDeVeiculo: parseInt(tipoVeiculo, 10),
+      marcaDoVeiculo: marca,
+      modeloDeVeiculo: modelo,
+      portasDoVeiculo: qtdPortas,
     };
 
     try {
@@ -45,24 +51,6 @@ const NewCar = () => {
       <div className="newCar">
         <h3>Cadastro de Veículo</h3>
         <form onSubmit={handleSubmit}>
-          <div className="inputGroup">
-            <input
-              type="text"
-              required
-              value={placa}
-              onChange={(e) => setPlaca(e.target.value)}
-              placeholder="placa"
-              className="width50"
-            />
-            <input
-              type="text"
-              required
-              value={cor}
-              onChange={(e) => setCor(e.target.value)}
-              placeholder="cor"
-              className="width50"
-            />
-          </div>
           <div className="inputGroup">
             <input
               type="text"
@@ -90,6 +78,58 @@ const NewCar = () => {
               <option value={1}>Passeio</option>
               <option value={2}>Familiar</option>
               <option value={3}>Esportivo</option>
+            </select>
+          </div>
+          <div className="inputGroup">
+            <input
+              type="text"
+              required
+              value={placa}
+              onChange={(e) => setPlaca(e.target.value)}
+              placeholder="placa"
+              className="width33"
+            />
+            <input
+              type="text"
+              required
+              value={cor}
+              onChange={(e) => setCor(e.target.value)}
+              placeholder="cor"
+              className="width33"
+            />
+            <select
+              required
+              onChange={(e) => setQtdPortas(e.target.value)}
+              className="width33"
+            >
+              <option>qtd portas</option>
+              <option value="DUAS">2</option>
+              <option value="QUATRO">4</option>
+            </select>
+          </div>
+          <div className="inputGroup">
+            <select
+              required
+              onChange={(e) => setMarca(e.target.value)}
+              className="width50"
+            >
+              <option>Marca</option>
+              <option value="PEUGEOT">Peugeot</option>
+              <option value="HONDA">Honda</option>
+              <option value="VOLKSWAGEN">Volkswagen</option>
+            </select>
+            <select
+              required
+              onChange={(e) => setModelo(e.target.value)}
+              className="width50"
+            >
+              <option>Modelo</option>
+              <option value="3008">3008</option>
+              <option value="208">208</option>
+              <option value="FOX">Fox</option>
+              <option value="SAVEIRO">Saveiro</option>
+              <option value="FIT">Fit</option>
+              <option value="CIVIC">Civic</option>
             </select>
           </div>
           <div className="inputGroup">
