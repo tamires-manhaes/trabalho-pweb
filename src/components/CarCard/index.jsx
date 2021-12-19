6d6e23ddfb73eea0f3f1b0811e930b797fe7ca5d
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-19 15:49:25 -0300

    chore: add car props

--- src/components/CarCard/index.jsx
+++ src/components/CarCard/index.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './styles.css';
 
-const CarCard = ({
-  id,
-  tipoVeiculo,
-  cor
-}) => {
+const CarCard = ({ id, tipoVeiculo, cor, qtdPortas, marca, modelo }) => {
   const history = useHistory();
 
-  const deleteThis = (id) => {
+  const details = (id) => {
     return history.push(`/car-details/${id}`);
   };
 
   return (
     <li className="carCard">
-      <h3>{tipoVeiculo}</h3>
+      <h3>
+        {marca} {modelo}
+      </h3>
+      <span>Tipo: {tipoVeiculo}</span>
       <span>Cor: {cor}</span>
-      <button onClick={() => deleteThis(id)}>Mais informações</button>
+      <span>Qtd portas: {qtdPortas}</span>
+      <button onClick={() => details(id)}>Mais informações</button>
     </li>
   );
 };
