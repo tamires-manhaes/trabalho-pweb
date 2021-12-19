062b85b066f9574a01416539af8388a33d81056f
Tamires <tamiresmanhaes4@gmail.com>
2021-12-19 11:51:40 -0300

    chore: displaying data on home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,55 +2,111 @@ import React, { useEffect, useState } from 'react';
 import Container from '../../components/Container';
 import { useHistory } from 'react-router-dom';
 import CarCard from '../../components/CarCard';
+import EmployeeCard from '../../components/EmployeeCar';
+import UsuarioCard from '../../components/UsuarioCard';
 import api from '../../service/index';
 import './styles.css';
 
 export const Home = () => {
   const history = useHistory();
-  const [cars, setCars] = useState({});
+  const [cars, setCars] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    api.get('/cars').then((response) => {
+    api.get('/veiculos').then((response) => {
       setCars(response.data);
-      console.log(cars);
     });
   }, []);
 
+  useEffect(() => {
+    api.get('/funcionarios').then((response) => {
+      setEmployees(response.data);
+    });
+  },[])
+
+  useEffect(() => {
+    api.get('/usuarios').then((response) => {
+      console.log(response.data)
+      setUsers(response.data);
+    });
+  },[])
+
   const redirectToLogin = () => {
-    return history.push('/login');
+    return history.push('/newUser');
   };
 
   return (
-    <div className="homePage">
-      <Container
+    <Container
         title="PWEB Cars"
         onClick={redirectToLogin}
         showButton
-        buttonTittle="Login"
+        buttonTittle="Dashboard"
       >
-        <ul className="carList">
-          {/* {cars ? (
-            cars.map((car) => {
-              return (
-                <CarCard
-                  key={car.id}
-                  id={car.id}
-                  name={car.name}
-                  cor={car.cor}
-                  classificationGroup={car.classificationGroup.description}
-                  qtdSits={car.qtdSits}
-                  kmsDriven={car.kmsDriven}
-                  available={car.available}
-                  qtdFuel={car.qtdFuel}
-                  foto={car.foto}
-                />
-              );
-            })
-          ) : (
-            <span>nenhum carro a ser exibido</span>
-          )} */}
-        </ul>
-      </Container>
-    </div>
+      <div className="homePage">
+        <div className="cardsBox">
+          <h3>Carros disponíveis</h3>
+          <ul className="carList">
+            {cars ? (
+              cars.map((car) => {
+                return (
+                  <CarCard
+                    key={car.id}
+                    id={car.id}
+                    cor={car.cor}
+                    tipoVeiculo={car.tipoDeVeiculo}
+                  />
+                );
+              })
+            ) : (
+              <span>nenhum carro a ser exibido</span>
+            )}
+          </ul>
+        </div>
+
+        <div className="cardsBox">
+          <h3>Funcionarios disponíveis</h3>
+          <ul className="carList">
+            {employees ? (
+              employees.map((emp) => {
+                return (
+                  <EmployeeCard
+                    key={emp.id}
+                    id={emp.id}
+                    nome={emp.nome} 
+                    email={emp.email} 
+                    matricula={emp.matricula}
+                  />
+                );
+              })
+            ) : (
+              <span>nenhum carro a ser exibido</span>
+            )}
+          </ul>
+        </div>
+
+        <div className="cardsBox">
+          <h3>Funcionarios disponíveis</h3>
+          <ul className="carList">
+            {users ? (
+              users.map((user) => {
+                return (
+                  <UsuarioCard
+                    key={user.id}
+                    id={user.id}
+                    nome={user?.funcionario?.nome} 
+                    email={user?.funcionario?.email} 
+                    matricula={user?.funcionario?.matricula}
+                    login={user?.login}
+                  />
+                );
+              })
+            ) : (
+              <span>nenhum carro a ser exibido</span>
+            )}
+          </ul>
+        </div>
+      </div>
+    </Container>
   );
 };
