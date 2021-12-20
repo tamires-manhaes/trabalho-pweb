1aba769a9cf8260be6046f058e47c7639b1fb83b
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-20 12:36:18 -0300

    chore: adjusting employee card

--- src/components/EmployeeCar/index.jsx
+++ src/components/EmployeeCar/index.jsx
@@ -1,21 +1,23 @@
 import { useHistory } from 'react-router-dom';
-import './styles.css'
+import './styles.css';
 
-const EmployeeCard = ({id, nome, email, matricula}) => {
+const EmployeeCard = ({ id, nome, email, matricula }) => {
   const history = useHistory();
 
-  const deleteThis = (id) => {
-    return history.push(`/car-details/${id}`);
+  const redirectToDetails = (id) => {
+    return history.push(`/employee-details/${id}`);
   };
 
   return (
     <li className="employeeCard">
-      <h3>{nome}</h3>
-      <span>Email: {email}</span>
-      <span>Matricula: {matricula}</span>
-      <button onClick={() => deleteThis(id)}>Mais informações</button>
+      <div className="infos">
+        <h3>{nome}</h3>
+        <span>Email: {email}</span>
+        <span>Matricula: {matricula}</span>
+      </div>
+      <button onClick={() => redirectToDetails(id)}>Mais informações</button>
     </li>
-  )
-}
+  );
+};
 
-export default EmployeeCard
+export default EmployeeCard;
