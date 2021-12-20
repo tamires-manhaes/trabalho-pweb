59589ccfefc164832d01c906373b09597f957254
Tamires <tamires.manhaes@ifba.edu.br>
2021-12-20 12:36:02 -0300

    chore: ajuste car card props

--- src/components/CarCard/index.jsx
+++ src/components/CarCard/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import {
+  getCarBrand,
+  getCarModel,
+  getCarDoors,
+  getCarType,
+} from '../../utils/car';
 import './styles.css';
 
 const CarCard = ({ id, tipoVeiculo, cor, qtdPortas, marca, modelo }) => {
@@ -9,14 +15,21 @@ const CarCard = ({ id, tipoVeiculo, cor, qtdPortas, marca, modelo }) => {
     return history.push(`/car-details/${id}`);
   };
 
+  let marcaFormated = getCarBrand(marca);
+  let modeloFormated = getCarModel(modelo);
+  let qtdPortasFormated = getCarDoors(qtdPortas);
+  let tipo = getCarType(tipoVeiculo);
+
   return (
     <li className="carCard">
-      <h3>
-        {marca} {modelo}
-      </h3>
-      <span>Tipo: {tipoVeiculo}</span>
-      <span>Cor: {cor}</span>
-      <span>Qtd portas: {qtdPortas}</span>
+      <div className="infos">
+        <h3>
+          {marcaFormated} {modeloFormated}
+        </h3>
+        <span>Tipo: {tipo}</span>
+        <span>Cor: {cor}</span>
+        <span>Qtd portas: {qtdPortasFormated}</span>
+      </div>
       <button onClick={() => details(id)}>Mais informações</button>
     </li>
   );
